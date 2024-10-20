ea7e13c19d0ec7cf689ebe31c8e16a5e1f83b6e5
Fatim-coding <timabadjie12@gmail.com>
2024-10-20 15:15:10 +0000

    second commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# storing values
+tree1 = 35
+tree2 = 28
+tree3 = 97
+tree4 = 10
+tree5 = 56
+
+# finding the total of trees
+sum = tree1+tree2+tree3+tree4+tree5
+print("the sum of all the 5 tree is:", sum)
+
+# finding the averagemof tree
+average = sum/5
+print("the average of all the tree is:",average)
